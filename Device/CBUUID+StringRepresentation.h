96b54594d5804b31362b6a9f29878500905d3a50
Kamil Trzcinski <ayufan@ayufan.eu>
2015-02-04 14:07:32 +0100

    Final version

---
+++ Device/CBUUID+StringRepresentation.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface CBUUID (StringRepresentation)
+- (NSString *)stringRepresentation;
+@end
--- RemoteUnlockEstimote/RUAppDelegate.m
+++ RemoteUnlockEstimote/RUAppDelegate.m
@@ -7,9 +7,9 @@
 //
 
 #import "RUAppDelegate.h"
-#import "ESTBeaconManager.h"
-#import "ESTBeaconRegion.h"
-#import "ESTBeacon.h"
+#import "JSTSensorManager.h"
+#import "JSTSensorTag.h"
+#import "JSTMagnetometerSensor.h"
 #import <WebKit/WebKit.h>
 
 #define BEACON_UUID @"f1eed054ac4e"
@@ -18,9 +18,9 @@
 #define LOCK_DELAY 2.0
 #define UNLOCK_DELAY 0.5
 
-@interface RUAppDelegate () <ESTBeaconManagerDelegate, ESTBeaconDelegate>
+@interface RUAppDelegate () <JSTSensorManagerDelegate, JSTBaseSensorDelegate>
 
-@property (nonatomic, strong) ESTBeaconManager* beaconManager;
+@property (nonatomic, strong) JSTSensorManager *sensorManager;
 
 @property (nonatomic, strong) NSTimer * unlockTimer;
 @property (nonatomic, strong) NSTimer * lockTimer;
@@ -29,73 +29,153 @@
 
 @end
 
+static bool connected = false;
+
 @implementation RUAppDelegate
 
+- (void)manager:(JSTSensorManager *)manager didConnectSensor:(JSTSensorTag *)sensor
+{
+    // NSLog(@"Sensor connected: %@", sensor.macAddress);
+
+    sensor.magnetometerSensor.sensorDelegate = self;
+    [sensor.magnetometerSensor configureWithValue:JSTSensorMagnetometerEnabled];
+    [sensor.magnetometerSensor setPeriodValue:10];
+    [sensor.magnetometerSensor setNotificationsEnabled:YES];
+}
+
+- (void)manager:(JSTSensorManager *)manager didDisconnectSensor:(JSTSensorTag *)sensor error:(NSError *)error
+{
+    connected = false;
+
+//    connected = true;
+//    [self.sensorManager connectSensorWithUUID:
+//            [[NSUUID alloc] initWithUUIDString:@"063A3DBD-2002-46B7-9AC9-98862CA8042F"]];
+}
+
+- (void)manager:(JSTSensorManager *)manager didFailToConnectToSensorWithError:(NSError *)error
+{
+    connected = false;
+//    connected = true;
+//    [self.sensorManager connectSensorWithUUID:
+//            [[NSUUID alloc] initWithUUIDString:@"063A3DBD-2002-46B7-9AC9-98862CA8042F"]];
+}
+
+- (void)manager:(JSTSensorManager *)manager didDiscoverSensor:(JSTSensorTag *)sensor
+{
+    if (!sensor) {
+        return;
+    }
+
+    if ([sensor.macAddress isEqualToString:@"BC:6A:29:AB:45:79"]) {
+        if (![sensor.magnetometerSensor canBeConfigured]) {
+            NSLog(@"Can't be configured");
+            return;
+        }
+    }
+
+    //  NSLog(@"CB: %@, RSSI: %d", sensor.peripheral, sensor.rssi);
+
+//    if (!connected) {
+//    }
+}
+
+- (void)manager:(JSTSensorManager *)manager didChangeStateTo:(CBCentralManagerState)state
+{
+    static bool started_scanning = false;
+    
+    if(!started_scanning && state == CBCentralManagerStatePoweredOn) {
+        started_scanning = true;
+        // [self.sensorManager startScanning];
+    }
+}
+
+
+- (void)sensorDidUpdateValue:(JSTBaseSensor *)sensor {
+    static bool isCalibrated = false;
+    if ([sensor isKindOfClass:[JSTMagnetometerSensor class]]) {
+        JSTMagnetometerSensor *magnetometerSensor = (JSTMagnetometerSensor *) sensor;
+        if (!isCalibrated) {
+            isCalibrated = YES;
+            [magnetometerSensor calibrate];
+        } else {
+            float length = magnetometerSensor.value.x * magnetometerSensor.value.x
+                    + magnetometerSensor.value.y * magnetometerSensor.value.y
+                    + magnetometerSensor.value.z * magnetometerSensor.value.z;
+            length = sqrtf(length);
+
+            NSLog(@"Length: %f", length);
+
+            static bool isLocked = false;
+
+            static bool lastToggled = false;
+            bool shouldToggle = false;
+
+            if(length > 500) {
+                shouldToggle = true;
+            }
+
+            if (shouldToggle && lastToggled != shouldToggle) {
+                if (!isLocked) {
+                    isLocked = true;
+                    [self lock];
+                } else {
+                    isLocked = false;
+                    [self unlock];
+                }
+            }
+
+            lastToggled = shouldToggle;
+        }
+    }
+}
+
+- (void)sensorDidFailCommunicating:(JSTBaseSensor *)sensor withError:(NSError *)error {
+
+}
+
+- (void)sensorDidFinishCalibration:(JSTBaseSensor *)sensor {
+
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    
+    self.sensorManager = [JSTSensorManager new];
+    self.sensorManager.delegate = self;
+
+    connected = true;
+    [self.sensorManager connectSensorWithUUID:
+            [[NSUUID alloc] initWithUUIDString:@"063A3DBD-2002-46B7-9AC9-98862CA8042F"]];
+
     NSRect screenRect = [[NSScreen mainScreen] frame];
     
     WebView *webView = [[WebView alloc] init];
-    [webView setHidden:FALSE];
     
     NSString* filePath = [[NSBundle mainBundle] pathForResource:@"lock"
                                                          ofType:@"html"
                                                     inDirectory:@""];
     NSURL* fileURL = [NSURL fileURLWithPath:filePath];
-    //    NSURL *url = [NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/858551/lock.html"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:fileURL];
     [[webView mainFrame] loadRequest:urlRequest];
     [webView setFrame:screenRect];
+
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyAccessory];
     
     [self.window setContentView:webView];
     
     [self.window setStyleMask:NSBorderlessWindowMask];
-//    [self.window setBackingType:NSBackingStoreBuffered];
     [self.window setFrame:[[NSScreen mainScreen] frame] display:NO animate:NO];
     
     [self.window setLevel:NSScreenSaverWindowLevel];
     [self.window makeKeyAndOrderFront:nil];
     
     [self.window miniaturize:self];
-    
-    self.beaconManager = [[ESTBeaconManager alloc] init];
-    self.beaconManager.delegate = self;
-    
-    ESTBeaconRegion *region = [[ESTBeaconRegion alloc] initRegionWithIdentifier:@"dd"];file:///Users/ayufan/Library/Developer/Xcode/DerivedData/RemoteUnlockEstimote-ftcyavmbtwdtasgfnlupkotoewte/Build/Products/Debug/RemoteUnlockEstimote.app/Contents/Resources/background.jpg
-    [self.beaconManager startEstimoteBeaconsDiscoveryForRegion:region];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification
 {
-    if(self.locked) {
-        system("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend");
-    }
-}
-
--(void)beaconManager:(ESTBeaconManager *)manager
-  didDiscoverBeacons:(NSArray *)beacons
-            inRegion:(ESTBeaconRegion *)region
-{
-    ESTBeacon * beacon = nil;
-    
-    for(int i = 0; i < [beacons count]; ++i) {
-        ESTBeacon * current = (ESTBeacon*)[beacons objectAtIndex:i];
-        if([current.macAddress isEqualToString:BEACON_UUID]) {
-            beacon = current;
-            break;
-        }
-    }
-    
-    if(beacon) {
-        NSLog(@"BeaconFound(%i): mac:%@ power:%f rssi:%f", [beacons count], beacon.macAddress, [beacon.measuredPower floatValue], [beacon.rssi floatValue]);
-    } else {
-        // NSLog(@"BeaconNotFound");
-    }
-
-    [self scheduleLockScreen:beacon.rssi];
-    [self scheduleUnlockScreen:beacon.rssi];
+//    if(self.locked) {
+//        system("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend");
+//    }
 }
 
 - (void) scheduleLockScreen:(NSNumber*)rssi
@@ -193,7 +273,6 @@
     //system("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -switchToUserID 501");
     //system("defaults write com.apple.screensaver askForPassword 1");
     
-    
     [self unlock];
 }
 
@@ -203,7 +282,6 @@
     [sound play];
     
     [[self window] miniaturize:self];
-    
 }
 
 - (void)lock {
